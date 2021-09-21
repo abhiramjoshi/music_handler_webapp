e7e000d183b8e4285b703433aa71c5b02b5adbe7
Abhi_Joshi <joshi.abhiram98@gmail.com>
2021-09-21 18:21:24 -0400

    postgres_database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,9 +82,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': POSTGRES['DB_NAME'],
+
+        'USER': POSTGRES['DB_USER'],
+
+        'PASSWORD': POSTGRES['DB_PASSWORD'],
+
+        'HOST': POSTGRES['HOST'],
+
+        'PORT': POSTGRES['PORT'],
+
+    },
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 LOGIN_REDIRECT_URL = '/'
 
