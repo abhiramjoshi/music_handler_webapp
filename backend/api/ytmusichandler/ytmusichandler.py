e4258919994bfeb4dc6de88e36bca39068a51c61
Abhi_Joshi <joshi.abhiram98@gmail.com>
2021-12-02 23:52:46 +0000

    youtube merge playlists

--- backend/api/ytmusichandler/ytmusichandler.py
+++ backend/api/ytmusichandler/ytmusichandler.py
@@ -18,7 +18,7 @@ class YTMusicHandler():
     def sync_local_library(self):
         self.playlists = self.get_playlist()
 
-    def get_playlist(self):
+    def get_all_playlists(self):
         try:
             playlists = self.servicemanager.get_library_playlists()
             #use the get playlist method to get details of each user playlist. Need description, need url.
@@ -40,22 +40,76 @@ class YTMusicHandler():
             if '403' in str(e):
                 raise HeadersAuthenticationError
 
-    def remove_duplicates(self, playlist):
-        songs = {}
-        raw_songs = self.servicemanager.get_playlist(playlist['id'], playlist['count'])['tracks']
-        for song in raw_songs:
-            songs.update({song["videoID"]:song})
-        return list(songs.values())
+    def get_playlist(self, playlist):
+        playlist_obj = self.servicemanager.get_playlist(playlist, limit=1)
+        track_count = playlist_obj["trackCount"]
+        playlist_obj = self.servicemanager.get_playlist(playlist, limit=track_count)
+        return playlist_obj
 
-    def merge_playlists(playlists_to_merge):
-        merged_playlist = {}
-        for playlist in playlists_to_merge:
-            playlist_tracks = {track['videoID']: track for track in playlist['tracks']}
-            merged_playlist.update(playlist_tracks)
+    def get_playlist_tracks(self, playlist):
+        """
+        Returns playlist track IDs (videoIds)
+        """
+        playlist = self.servicemanager.get_playlist(playlist)
+        id = [track['videoId'] for track in playlist['tracks']]
+        return id
 
+    def create_playlist(self, title, description, video_ids, privacy_status='PRIVATE'):
+        return self.servicemanager.create_playlist(title, description, privacy_status, video_ids)
 
+    def add_tracks(self, playlist, tracklist):
+        """
+        Add songs to an existing playlist
 
+        playlistId: Playlist id
+        videoIds: List of Video ids
+        """
+        return self.servicemanager.add_playlist_items(playlist, tracklist)
 
+    def remove_duplicates(self, playlists, primary_pl=None):
+        """
+        Remove duplicates from a set of playlists. Returns a set of collated playlist
+        tracks with duplicates removed as well as a set of tracks that that are not 
+        present in the 'primary playlist' provided.
+
+        playlists: list of playlist IDs from which duplicates will be removed
+        primary_pl: specify the 'primary playlist' in the provided set. 
+        """
+        if primary_pl is None:
+            primary_pl = playlists[0]
+
+        tracks_present = set(self.get_playlist_tracks(primary_pl))    
+        tracks_to_add = []
+        for playlist in playlists:
+            if playlist == primary_pl:
+                continue
+            tracks = self.get_playlist_tracks(playlist) #get tracklist 
+            tracks_to_add.extend(set(tracks).difference(tracks_present)) #record unaccounted for tracks
+            tracks_present.update(tracks_to_add) #update unaccounted for tracks
+        return tracks_present, tracks_to_add
+
+    def merge_playlists(self, playlists_to_merge, merge_into = None, 
+                        new_playslist_name=None, new_playlist_description=None, 
+                        delete=False):
+        """
+        Merge playlists
+
+        playlists_to_merge: List of playlist ids List[str]
+        merge_into: Id of playlist to merge into, additional songs will be added to this playlist
+        new_playslist_name: Name of newly created playlist (if not merging into existing)
+        new_playslist_description: Description of newly created playlist
+        delete: Delete playlists after merged
+        """
+        print('YT')
+        if merge_into is not None:
+            #add tracks to main playlist
+            _, tracks_to_add = self.remove_duplicates(playlists_to_merge, merge_into)
+            return self.add_tracks(merge_into, tracks_to_add)
+        else:
+            # create new playlist
+            tracklist, _ = self.remove_duplicates(playlists_to_merge)
+            
+            #return self.create_playlist(new_playslist_name, new_playlist_description, list(tracklist))
 
 if __name__ == "__main__":
     pass
